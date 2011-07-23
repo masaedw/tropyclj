d44ad0c4e02203a99e937c79164481218e96a105
masaedw <masa.edw@gmail.com>
2011-07-23 17:04:08 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject tropyclj "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0"]
+                 [compojure "0.6.2"]
+                 [hiccup "0.3.4"]
+                 [ring "0.3.7"]
+                 ])
