87846e39ff12a9983daba563cee2fd64dfaf7652
masaedw <masa.edw@gmail.com>
2011-07-23 18:20:02 +0900

    maybe-init take one or zero arguments

--- src/tropyclj/mongo_init.clj
+++ src/tropyclj/mongo_init.clj
@@ -8,15 +8,18 @@
     (when (.find matcher) ;; Check if it matches.
       (zipmap [:match :user :pass :host :port :db] (re-groups matcher))))) ;; Construct an options map.
 
-(defn maybe-init []
+(defn maybe-init
   "Checks if connection and collection exist, otherwise initialize."
-  (when (not (connection? *mongo-config*)) ;; If global connection doesn't exist yet.
-    (let [mongo-url (get (System/getenv) "MONGOHQ_URL") ;; Heroku puts it here.
-	  config    (split-mongo-url mongo-url)] ;; Extract options.
-      (println "Initializing mongo @ " mongo-url)
-      (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config))) ;; Setup global mongo.
-      (authenticate (:user config) (:pass config)) ;; Setup u/p.
-      (or (collection-exists? :firstcollection) ;; Create collection named 'firstcollection' if it doesn't exist.
-	  (create-collection! :firstcollection)))))
+  ([url]
+     (when (not (connection? *mongo-config*)) ;; If global connection doesn't exist yet.
+       (let [mongo-url (get (System/getenv) "MONGOHQ_URL" url) ;; Heroku puts it here.
+             config    (split-mongo-url mongo-url)] ;; Extract options.
+         (println "Initializing mongo @ " mongo-url)
+         (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config))) ;; Setup global mongo.
+         (authenticate (:user config) (:pass config)) ;; Setup u/p.
+         (or (collection-exists? :firstcollection) ;; Create collection named 'firstcollection' if it doesn't exist.
+             (create-collection! :firstcollection)))))
+  ([]
+     (maybe-init "")))
 
 (def mongo-init maybe-init)
