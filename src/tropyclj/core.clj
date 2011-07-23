7252a725e8693b3eb3bd483d959df2323c36cf37
masaedw <masa.edw@gmail.com>
2011-07-24 00:19:30 +0900

    compojure

--- src/tropyclj/core.clj
+++ src/tropyclj/core.clj
@@ -1,4 +1,8 @@
 (ns tropyclj.core
+  (:use clojure.contrib.pprint)
+  (:use compojure.core)
+  (:use compojure.handler)
+  (:use compojure.route)
   (:use ring.adapter.jetty)
   (:use ring.middleware.reload)
   (:use ring.middleware.stacktrace)
@@ -6,18 +10,31 @@
   (:use tropyclj.mongo-init)
 )
 
-(mongo-init)
-
 (defn handler [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body "Hello from Clojure!\n"})
 
-(def app
-     (-> #'handler
-         (wrap-reload '(tropyclj.core))
-         (wrap-stacktrace)))
+(defn wrap-charset [handler charset]
+  (fn [request]
+    (if-let [response (handler request)]
+      (if-let [content-type (get-in response [:headers "Content-Type"])]
+        (if (.contains content-type "charset")
+          response
+          (assoc-in response
+                    [:headers "Content-Type"]
+                    (str content-type "; charset=" charset)))
+        response))))
+
+(defroutes tropyclj
+  (GET "/" req (handler req))
+  (not-found "not found"))
+
+(wrap! tropyclj (:charset "utf8"))
+(wrap! tropyclj (:reload '(tropyclj.core)))
+(wrap! tropyclj (:stacktrace))
 
 (defn -main []
+  (mongo-init)
   (let [port (Integer/parseInt (System/getenv "PORT"))]
-    (run-jetty #'app {:port port})))
+    (run-jetty #'tropyclj {:port port})))
